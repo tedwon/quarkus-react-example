d0dcf6d6d3fc666523275b6a8f0f3ef892c8f909
Ted Won <jwon@redhat.com>
2022-11-19 12:50:52 +1000

    Add webapp

---
+++ src/main/webapp/src/App.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import Particles from './components/particles'
+
+class App extends Component {
+  state = {
+    particles: []
+  }
+
+  componentDidMount() {
+//    fetch('/particles')
+    fetch('http://localhost:8080/particles')
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({ particles: data })
+    })
+    .catch(console.log)
+  }
+
+  render () {
+    return (
+      <Particles particles={this.state.particles} />
+    );
+  }
+}
+
+export default App;
